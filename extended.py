c8f0e0dfc728238871a046f18b3a089b67bfea0a
yoku-nai-desu <mukoka@tutamail.com>
2024-05-19 18:13:47 +0330

    don't use lxml, switch to built-in html-parser - also try to simulate new session without being cached

--- extended.py
+++ extended.py
@@ -30,17 +30,21 @@ def is_captcha_response(response_text):
 
 def check_responses(urls, error_codes):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+        'Cache-Control': 'no-cache'
+    }
     
     for url in urls:
         try:
-            response = requests.get(url, headers=headers)
-            if response.status_code in error_codes:
-                print(f"{url} = {response.status_code}, Message = \"{get_random_line('error_messages.txt')}\"")
-            elif is_captcha_response(response.text):
-                print(f"{url} = CAPTCHA detected, please verify manually.")
-            else:
-                print(f"{url} = {response.status_code}")
+            with requests.Session() as session:
+                session.headers.update(headers)
+                response = session.get(url)
+                if response.status_code in error_codes:
+                    print(f"{url} = {response.status_code}, Message = \"{get_random_line('error_messages.txt')}\"")
+                elif is_captcha_response(response.text):
+                    print(f"{url} = CAPTCHA detected, please verify manually.")
+                else:
+                    print(f"{url} = {response.status_code}")
         except requests.exceptions.RequestException as e:
             print(f"{url} = Error: {e}")
 
